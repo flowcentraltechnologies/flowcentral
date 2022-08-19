1443c6737a512f369895cf182155d3ce3af724e3
Lateef Ojulari <lateef.ojulari@tcdng.com>
2022-08-19 01:49:10 +0100

    Implement manage dashboard status

--- flowcentral-dashboard/src/main/java/com/flowcentraltech/flowcentral/dashboard/policies/ActivateDashboardsActionPolicy.java
+++ flowcentral-dashboard/src/main/java/com/flowcentraltech/flowcentral/dashboard/policies/ActivateDashboardsActionPolicy.java
@@ -15,19 +15,10 @@
  */
 package com.flowcentraltech.flowcentral.dashboard.policies;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.flowcentraltech.flowcentral.common.business.policies.AbstractEntityListActionPolicy;
-import com.flowcentraltech.flowcentral.common.business.policies.EntityListActionContext;
-import com.flowcentraltech.flowcentral.common.business.policies.EntityListActionResult;
 import com.flowcentraltech.flowcentral.common.constants.RecordStatus;
-import com.flowcentraltech.flowcentral.dashboard.business.DashboardModuleService;
 import com.flowcentraltech.flowcentral.dashboard.entities.DashboardQuery;
 import com.tcdng.unify.core.UnifyException;
 import com.tcdng.unify.core.annotation.Component;
-import com.tcdng.unify.core.annotation.Configurable;
-import com.tcdng.unify.core.database.Entity;
 
 /**
  * Activate dashboards action policy.
@@ -36,28 +27,12 @@ import com.tcdng.unify.core.database.Entity;
  * @since 1.0
  */
 @Component("activatedashboards-actionpolicy")
-public class ActivateDashboardsActionPolicy extends AbstractEntityListActionPolicy {
-
-	@Configurable
-	private DashboardModuleService dashboardModuleService;
-
-	public void dashboardModuleService(DashboardModuleService dashboardModuleService) {
-		this.dashboardModuleService = dashboardModuleService;
-	}
+public class ActivateDashboardsActionPolicy extends AbstractDashboardEntityListActionPolicy {
 
 	@Override
-	public EntityListActionResult executeAction(EntityListActionContext ctx) throws UnifyException {
-		List<Long> idList = new ArrayList<Long>();
-		for (Entity entity : ctx.getInstList()) {
-			idList.add((Long) entity.getId());
-		}
-
-		if (!idList.isEmpty()) {
-			dashboardModuleService.updateDashboardStatus((DashboardQuery) new DashboardQuery().idIn(idList),
-					RecordStatus.ACTIVE);
-		}
-
-		return null;
+	protected void doExecuteAction(DashboardQuery query) throws UnifyException {
+		dashboard().updateDashboardStatus(query,
+				RecordStatus.ACTIVE);
 	}
 
 }
