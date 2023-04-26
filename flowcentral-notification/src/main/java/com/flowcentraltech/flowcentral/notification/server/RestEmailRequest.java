2d711e1f131116d1ffbd5da1a951be06b681b155
Lateef Ojulari <lateef.ojulari@tcdng.com>
2023-04-26 21:36:12 +0100

    Implement REST email server

---
+++ flowcentral-notification/src/main/java/com/flowcentraltech/flowcentral/notification/server/RestEmailRequest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021-2023 FlowCentral Technologies Limited.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.flowcentraltech.flowcentral.notification.server;
+
+import java.util.List;
+
+/**
+ * REST email request object.
+ * 
+ * @author FlowCentral Technologies Limited
+ * @since 1.0
+ */
+public class RestEmailRequest {
+
+    private List<String> to;
+
+    private List<String> cc;
+
+    private List<String> bcc;
+    
+    private String subject;
+    
+    private String body;
+
+    public List<String> getTo() {
+        return to;
+    }
+
+    public void setTo(List<String> to) {
+        this.to = to;
+    }
+
+    public List<String> getCc() {
+        return cc;
+    }
+
+    public void setCc(List<String> cc) {
+        this.cc = cc;
+    }
+
+    public List<String> getBcc() {
+        return bcc;
+    }
+
+    public void setBcc(List<String> bcc) {
+        this.bcc = bcc;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
