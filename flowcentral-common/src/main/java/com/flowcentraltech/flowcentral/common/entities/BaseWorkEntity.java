8af9f02089989cd59776ff0c7879650099e91ac5
Lateef Ojulari <lateef.ojulari@tcdng.com>
2023-08-20 15:45:38 +0100

    Add unique constraint base field

--- flowcentral-common/src/main/java/com/flowcentraltech/flowcentral/common/entities/BaseWorkEntity.java
+++ flowcentral-common/src/main/java/com/flowcentraltech/flowcentral/common/entities/BaseWorkEntity.java
@@ -26,7 +26,6 @@ import com.tcdng.unify.core.annotation.ForeignKey;
 import com.tcdng.unify.core.annotation.ListOnly;
 import com.tcdng.unify.core.annotation.Policy;
 import com.tcdng.unify.core.annotation.QueryRestriction;
-import com.tcdng.unify.core.annotation.UniqueConstraint;
 import com.tcdng.unify.core.annotation.UniqueConstraints;
 
 /**
@@ -37,8 +36,8 @@ import com.tcdng.unify.core.annotation.UniqueConstraints;
  */
 @Policy("basework-entitypolicy")
 @DefaultQueryRestrictions({ @QueryRestriction(field = "wfItemVersionType", value = "ORN") })
-//@UniqueConstraints(baseFields = { "originalCopyId" },
-//    value = { @UniqueConstraint(value = { "wfItemVersionType" }) })
+@UniqueConstraints(baseFields = { "originalCopyId" } /*,
+    value = { @UniqueConstraint(value = { "wfItemVersionType" }) }*/)
 public abstract class BaseWorkEntity extends BaseAuditEntity implements WorkEntity {
 
     @ForeignKey(name = "PROCESSING_STATUS", nullable = true)
