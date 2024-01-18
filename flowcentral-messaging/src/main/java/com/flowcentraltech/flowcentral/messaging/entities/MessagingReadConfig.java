8de7223d03af0bc433c53590cb31f3171019c8a3
Lateef Ojulari <lateef.ojulari@tcdng.com>
2024-01-18 18:14:00 +0100

    Implement messaging (partial)

---
+++ flowcentral-messaging/src/main/java/com/flowcentraltech/flowcentral/messaging/entities/MessagingReadConfig.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2021-2023 FlowCentral Technologies Limited.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.flowcentraltech.flowcentral.messaging.entities;
+
+import com.flowcentraltech.flowcentral.common.entities.BaseStatusEntity;
+import com.tcdng.unify.core.annotation.Column;
+import com.tcdng.unify.core.annotation.Table;
+import com.tcdng.unify.core.annotation.UniqueConstraint;
+
+/**
+ * Messaging read configuration.
+ *
+ * @author FlowCentral Technologies Limited
+ * @since 1.0
+ */
+@Table(name = "FC_MESSAGINGREADCONFIG",
+        uniqueConstraints = {
+                @UniqueConstraint({ "name" }),
+                @UniqueConstraint({ "description" }) })
+public class MessagingReadConfig extends BaseStatusEntity {
+
+    @Column(name = "MESSAGINGREADCONFIG_NM", length = 64)
+    private String name;
+
+    @Column(name = "MESSAGINGREADCONFIG_DESC", length = 96)
+    private String description;
+
+    @Column(length = 64)
+    private String endpointConfig;
+
+    @Column(length = 64)
+    private String consumer;
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEndpointConfig() {
+        return endpointConfig;
+    }
+
+    public void setEndpointConfig(String endpointConfig) {
+        this.endpointConfig = endpointConfig;
+    }
+
+    public String getConsumer() {
+        return consumer;
+    }
+
+    public void setConsumer(String consumer) {
+        this.consumer = consumer;
+    }
+
+}
---
+++ flowcentral-messaging/src/main/java/com/flowcentraltech/flowcentral/messaging/entities/MessagingWriteConfig.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2021-2023 FlowCentral Technologies Limited.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.flowcentraltech.flowcentral.messaging.entities;
+
+import com.flowcentraltech.flowcentral.common.entities.BaseStatusEntity;
+import com.tcdng.unify.core.annotation.Column;
+import com.tcdng.unify.core.annotation.Table;
+import com.tcdng.unify.core.annotation.UniqueConstraint;
+
+/**
+ * Messaging write configuration.
+ *
+ * @author FlowCentral Technologies Limited
+ * @since 1.0
+ */
+@Table(name = "FC_MESSAGINGWRITECONFIG",
+        uniqueConstraints = {
+                @UniqueConstraint({ "name" }),
+                @UniqueConstraint({ "description" }) })
+public class MessagingWriteConfig extends BaseStatusEntity {
+
+    @Column(name = "MESSAGINGWRITECONFIG_NM", length = 64)
+    private String name;
+
+    @Column(name = "MESSAGINGWRITECONFIG_DESC", length = 96)
+    private String description;
+
+    @Column(length = 64)
+    private String endpointConfig;
+
+    @Column(length = 64)
+    private String producer;
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEndpointConfig() {
+        return endpointConfig;
+    }
+
+    public void setEndpointConfig(String endpointConfig) {
+        this.endpointConfig = endpointConfig;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+}
