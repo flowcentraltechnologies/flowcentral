e4175465c5297b59fe0b4301316a7511d7c83418
Lateef Ojulari <lateef.ojulari@tcdng.com>
2025-09-11 03:03:54 +0100

    Add OS messaging peer endpoint observer

---
+++ flowcentral-messaging-os/src/main/java/com/flowcentraltech/flowcentral/messaging/os/business/OSMessagingPeerEndpointObserver.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021-2025 FlowCentral Technologies Limited.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.flowcentraltech.flowcentral.messaging.os.business;
+
+import com.tcdng.unify.core.UnifyComponent;
+import com.tcdng.unify.core.UnifyException;
+
+/**
+ * OS messaging peer endpoint observer.
+ * 
+ * @author FlowCentral Technologies Limited
+ * @since 4.1
+ */
+public interface OSMessagingPeerEndpointObserver extends  UnifyComponent {
+
+    /**
+     * Fired on peer endpoint update.
+     * 
+     * @param appId
+     *                     the endpoint app ID
+     * @param endpointUrl
+     *                     the endpoint URL
+     * @param peerPassword
+     *                     the peer password
+     * @throws UnifyException
+     *                        if an error occurs
+     */
+    void update(String appId, String endpointUrl, String peerPassword) throws UnifyException;
+
+}
--- flowcentral-messaging-os/src/main/java/com/flowcentraltech/flowcentral/messaging/os/entities/OSMessagingPeerEndpoint.java
+++ flowcentral-messaging-os/src/main/java/com/flowcentraltech/flowcentral/messaging/os/entities/OSMessagingPeerEndpoint.java
@@ -19,6 +19,7 @@ import com.flowcentraltech.flowcentral.common.entities.BaseStatusEntity;
 import com.tcdng.unify.common.annotation.Table;
 import com.tcdng.unify.common.annotation.UniqueConstraint;
 import com.tcdng.unify.core.annotation.Column;
+import com.tcdng.unify.core.annotation.Policy;
 
 /**
  * OS messaging peer end-point.
@@ -26,6 +27,7 @@ import com.tcdng.unify.core.annotation.Column;
  * @author FlowCentral Technologies Limited
  * @since 4.1
  */
+@Policy("osmessagingpeerendpointpolicy")
 @Table(name = "FC_OSMESSAGINGPEERENDPOINT", uniqueConstraints = {
         @UniqueConstraint({ "appId" }),
         @UniqueConstraint({ "name" }),
