c90e43cf3218faa825517090cf60178a3d03bd40
Lateef Ojulari <lateef.ojulari@tcdng.com>
2026-06-08 00:56:16 +0100

    Re-implement change datasource view

--- flowcentral-studio/src/main/java/com/flowcentraltech/flowcentral/studio/web/panels/ChartDatasourceView.java
+++ flowcentral-studio/src/main/java/com/flowcentraltech/flowcentral/studio/web/panels/ChartDatasourceView.java
@@ -16,12 +16,12 @@
 
 package com.flowcentraltech.flowcentral.studio.web.panels;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.flowcentraltech.flowcentral.application.business.AppletUtilities;
 import com.flowcentraltech.flowcentral.application.data.EntityDef;
@@ -29,13 +29,10 @@ import com.flowcentraltech.flowcentral.application.data.TableDef;
 import com.flowcentraltech.flowcentral.application.web.widgets.BeanListTable;
 import com.flowcentraltech.flowcentral.application.web.widgets.BreadCrumbs;
 import com.flowcentraltech.flowcentral.chart.business.ChartModuleService;
-import com.flowcentraltech.flowcentral.chart.constants.ChartModuleNameConstants;
-import com.flowcentraltech.flowcentral.chart.data.AbstractSeries;
-import com.flowcentraltech.flowcentral.chart.data.AbstractSeries.AbstractSeriesData;
-import com.flowcentraltech.flowcentral.chart.data.ChartDataSourceDef;
-import com.flowcentraltech.flowcentral.chart.data.ChartDetails;
-import com.flowcentraltech.flowcentral.chart.data.ChartDetailsProvider;
-import com.flowcentraltech.flowcentral.chart.data.ChartTableColumn;
+import com.flowcentraltech.flowcentral.chart.data.CDSnapshot;
+import com.flowcentraltech.flowcentral.chart.data.CDSnapshotCategory;
+import com.flowcentraltech.flowcentral.chart.data.CDSnapshotSeries;
+import com.flowcentraltech.flowcentral.chart.data.ChartViewOption;
 import com.flowcentraltech.flowcentral.common.constants.ConfigType;
 import com.flowcentraltech.flowcentral.configuration.constants.EntityFieldDataType;
 import com.flowcentraltech.flowcentral.configuration.constants.EntityFieldType;
@@ -50,18 +47,14 @@ import com.tcdng.unify.core.data.ValueStore;
  * @since 4.1
  */
 public class ChartDatasourceView extends AbstractStudioEditorPage {
-    
+
     final ChartModuleService cms;
-    
+
     final String chartDatasourceName;
 
     private final Object baseId;
 
-    private BeanListTable dataSetTable;
-
-    private BeanListTable groupDataSetTable;
-
-    private ChartDetails chartDetails;
+    private BeanListTable snapshotTable;
 
     public ChartDatasourceView(AppletUtilities au, ChartModuleService cms, String chartDatasourceName, Object baseId,
             BreadCrumbs breadCrumbs) {
@@ -75,108 +68,81 @@ public class ChartDatasourceView extends AbstractStudioEditorPage {
         return baseId;
     }
 
-    public BeanListTable getDataSetTable() {
-        return dataSetTable;
+    public BeanListTable getSnapshotTable() {
+        return snapshotTable;
     }
 
-    public BeanListTable getGroupDataSetTable() {
-        return groupDataSetTable;
-    }
-
-    public ChartDetails getChartDetails() {
-        return chartDetails;
-    }
-
-    @SuppressWarnings("rawtypes")
     public void reloadContent() throws UnifyException {
-        final ChartDataSourceDef chartDataSourceDef = cms.getChartDataSourceDef(chartDatasourceName);
-        final ChartDetailsProvider detailsProvider = au().getComponent(ChartDetailsProvider.class,
-                ChartModuleNameConstants.CHARTDATASOURCE_PROVIDER);
-        chartDetails = detailsProvider.provide(chartDataSourceDef);
-        dataSetTable = null;
-        groupDataSetTable = null;
-        if (chartDetails.isWithSeries()) {
-            final Map<String, AbstractSeries<?, ?>> series = chartDetails.getSeries();
+        final CDSnapshot cdSnapshot = cms.getChartDatasourceSnapshot(chartDatasourceName, ChartViewOption.DEFAULT);
+        final CDSnapshotCategory[] categories = cdSnapshot.getCategories();
+        snapshotTable = null;
+        if (categories != null && categories.length > 0) {
+            // Structure
             final EntityDef.Builder edb = EntityDef.newBuilder(ConfigType.CUSTOM,
                     "com.flowcentraltech.flowcentral.preview.Az", "Preview", null, null, null, null, null, false, false,
                     false, false, false, "application.preview", "Preview", 1L, 1L);
             edb.addFieldDef("application.text", "application.text", EntityFieldDataType.STRING, EntityFieldType.STATIC,
-                    "series", "Series");
-            final Set<String> used = new HashSet<String>();
-            for (AbstractSeries<?, ?> _series : series.values()) {
-                for (AbstractSeriesData data : _series.getDataList()) {
-                    final String cat = String.valueOf(data.getX());
-                    final String catLabel = data.resolveX(data.getX());
-                    if (!used.contains(cat)) {
-                        if (_series.getDataType().isInteger()) {
-                            edb.addFieldDef("application.integer", "application.integer", EntityFieldDataType.INTEGER,
-                                    EntityFieldType.CUSTOM, cat, catLabel);
-                        } else {
-                            edb.addFieldDef("application.decimal", "application.decimal", EntityFieldDataType.DECIMAL,
-                                    EntityFieldType.CUSTOM, cat, catLabel);
-                        }
-                        used.add(cat);
+                    "category", "Category");
+            final CDSnapshotCategory category = categories[0];
+            if (category.getSeries() != null && category.getSeries().length > 0) {
+                for (CDSnapshotSeries series : category.getSeries()) {
+                    final int grouping = series.getGrouping();
+                    if (grouping == 0) {
+                        edb.addFieldDef("application.decimal", "application.decimal", EntityFieldDataType.DECIMAL,
+                                EntityFieldType.CUSTOM, series.getNm(),
+                                series.getLbl() != null ? series.getLbl() : series.getNm());
+                    } else if (grouping == 1) {
+                        edb.addFieldDef("application.text", "application.text", EntityFieldDataType.STRING,
+                                EntityFieldType.CUSTOM, series.getNm(),
+                                series.getLbl() != null ? series.getLbl() : series.getNm());
+                    } else {
+                        edb.addFieldDef("application.datetime", "application.datetime", EntityFieldDataType.TIMESTAMP,
+                                EntityFieldType.CUSTOM, series.getNm(),
+                                series.getLbl() != null ? series.getLbl() : series.getNm());
                     }
                 }
             }
 
             final EntityDef entityDef = edb.build(au());
-            final TableDef.Builder tdb = TableDef.newBuilder(entityDef, "Preview", false, false, "studio.dataSetTable",
+            final TableDef.Builder tdb = TableDef.newBuilder(entityDef, "Preview", false, false, "studio.snapshotTable",
                     "Priview Table", 0L, 0L);
-            tdb.sortHistory(4);
             tdb.itemsPerPage(-1);
 
+            // Data
             final List<ValueStore> list = new ArrayList<ValueStore>();
-            for (Map.Entry<String, AbstractSeries<?, ?>> entry : chartDetails.getSeries().entrySet()) {
-                Map<String, Object> map = new HashMap<String, Object>();
-                map.put("series", entry.getKey());
-                for (AbstractSeriesData data : entry.getValue().getDataList()) {
-                    map.put(String.valueOf(data.getX()), data.getY());
-                }
+            for (CDSnapshotCategory _category : categories) {
+                if (category.getSeries() != null && category.getSeries().length > 0) {
+                    final int len = category.getSeries()[0].getVals() != null ? category.getSeries()[0].getVals().length
+                            : 0;
+                    for (int i = 0; i < len; i++) {
+                        final Map<String, Object> map = new HashMap<String, Object>();
+                        map.put(_category.getCat(),
+                                _category.getLbl() != null ? _category.getLbl() : _category.getCat());
 
-                list.add(new MapValueStore(map));
+                        for (CDSnapshotSeries series : category.getSeries()) {
+                            final int grouping = series.getGrouping();
+                            final String val = series.getVals()[i];
+                            Object _val = null;
+                            if (val != null) {
+                                if (grouping == 0) {
+                                    _val = new BigDecimal(val);
+                                } else if (grouping == 1) {
+                                    _val = val;
+                                } else {
+                                    _val = new Date(Long.parseLong(val));
+                                }
+                            }
+
+                            map.put(series.getNm(), _val);
+                        }
+
+                        list.add(new MapValueStore(map));
+                    }
+                }
             }
 
-            dataSetTable = new BeanListTable(au(), tdb.build(au()));
-            dataSetTable.setSourceObjectClearSelected(list);
-        }
-
-        if (chartDetails.isWithTableSeries()) {
-            final EntityDef.Builder edb = EntityDef.newBuilder(ConfigType.CUSTOM,
-                    "com.flowcentraltech.flowcentral.preview.Az", "Preview", null, null, null, null, null, false, false,
-                    false, false, false, "application.preview", "Preview", 1L, 1L);
-            final ChartTableColumn[] headers = chartDetails.getTableHeaders();
-            for (ChartTableColumn col : headers) {
-                if (col.getType().isInteger()) {
-                    edb.addFieldDef("application.integer", "application.integer", col.getType(), EntityFieldType.CUSTOM,
-                            col.getFieldName(), col.getLabel());
-                } else if (col.getType().isDecimal()) {
-                    edb.addFieldDef("application.decimal", "application.decimal", col.getType(), EntityFieldType.CUSTOM,
-                            col.getFieldName(), col.getLabel());
-                } else {
-                    edb.addFieldDef("application.text", "application.text", col.getType(), EntityFieldType.CUSTOM,
-                            col.getFieldName(), col.getLabel());
-                }
-            }
-
-            final EntityDef entityDef = edb.build(au());
-            final TableDef.Builder tdb = TableDef.newBuilder(entityDef, "Preview", false, false, "studio.dataSetTable",
-                    "Priview Table", 0L, 0L);
-            tdb.sortHistory(4);
-            tdb.itemsPerPage(-1);
-
-            final List<ValueStore> list = new ArrayList<ValueStore>();
-            for (Object[] row : chartDetails.getTableSeries()) {
-                Map<String, Object> map = new HashMap<String, Object>();
-                for (int i = 0; i < headers.length; i++) {
-                    map.put(headers[i].getFieldName(), row[i]);
-                }
-
-                list.add(new MapValueStore(map));
-            }
-
-            groupDataSetTable = new BeanListTable(au(), tdb.build(au()));
-            groupDataSetTable.setSourceObjectClearSelected(list);
+            snapshotTable = new BeanListTable(au(), tdb.build(au()));
+            snapshotTable.setSourceObjectClearSelected(list);
         }
     }
 }
